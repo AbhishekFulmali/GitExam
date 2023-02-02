aba42d0655082931f745ac6673ffd1f9575e3d3a
Abhishek Fulmali <abhishekfulmali.33@gmail.com>
2023-02-02 17:26:47 +0530

    prime commit

--- Finbo.java
+++ Finbo.java
@@ -11,9 +11,32 @@ class Finbo{
               fib(count-1);
           }
     }
+
+    public static void prime(){
+         int ct = 0, n = 0, i = 1, j = 1;
+         
+         while (n < 20) {
+             j = 1;
+             ct = 0;
+             while (j <= i) {
+                 if (i % j == 0) {
+                     ct++;
+                     j++;
+                 }
+                 if (ct == 2) {
+                     System.out.println(" " + i);
+                     n++;
+                 }
+                 i++;
+             }
+         }
+       
+    }
     public static void main(String[] args) {
         int count =20;
         System.out.println(n1+" "+n2);
         fib(count-2);
+
+        
     }
 }
